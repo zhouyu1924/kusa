9e9ff6e84ed4927e7f018f593c05f6282cca6961
Tayler Khan <129487056+zhouyu1924@users.noreply.github.com>
2024-12-13 21:44:42 +0800

    Update k.bash

--- k.bash
+++ k.bash
@@ -2,38 +2,24 @@
 
 # 安装所需的软件
 sudo apt-get update
-sudo apt-get install -y git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+sudo apt-get install -y wget build-essential tar
 
-# 克隆 xmrig 代码
-git clone https://github.com/xmrig/xmrig.git
+# 下载和安装 WildRig Multi
+WILDRIG_VERSION="0.36.6"
+wget https://github.com/andru-kun/wildrig-multi/releases/download/$WILDRIG_VERSION/wildrig-multi-linux-$WILDRIG_VERSION.tar.xz
+mkdir -p wildrig && tar -xvf wildrig-multi-linux-$WILDRIG_VERSION.tar.xz -C wildrig --strip-components 1
 
-# 进入 xmrig 目录并构建
-cd xmrig || exit 1
-mkdir -p build && cd build || exit 1
-cmake ..
-make -j4
-
-# 创建配置文件 config.json
-cat << EOF > config.json
-{
-    "autosave": true,
-    "cpu": true,
-    "opencl": false,
-    "cuda": false,
-    "pools": [
-        {
-            "coin": "monero",
-            "algo": "rx/0",
-            "url": "stratum-eu.rplant.xyz:17084",
-            "user": "FM9VeF7jvEMxUPpdgKHLeLrFdRhN5atf5o",
-            "pass": "kusa",
-            "tls": false,
-            "keepalive": true,
-            "nicehash": false
-        }
-    ]
-}
+# 创建配置文件 run.sh
+cat << EOF > run.sh
+#!/bin/bash
+./wildrig-multi --algo ghostrider \
+  --url eu.miningpower.eu:22161 \
+  --user FS432tmzwmrhKoQVKx5Vxaa7hvrmQXvuNM \
+  --pass kusa \
+  --keepalive
 EOF
 
-# 使用 tmux 运行 xmrig
-tmux new -d -s xmrig './xmrig'
+chmod +x run.sh
+
+# 使用 tmux 运行 WildRig Multi
+tmux new -d -s wildrig './run.sh'
